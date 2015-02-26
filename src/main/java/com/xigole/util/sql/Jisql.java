9f5dcf66a014126a0cc2a014f8073cdcbd4660de
Scott Dunbar <scott@xigole.com>
2015-02-26 14:59:05 -0700

    cleanup

--- src/main/java/com/xigole/util/sql/Jisql.java
+++ src/main/java/com/xigole/util/sql/Jisql.java
@@ -5,7 +5,6 @@ import java.io.Console;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
